a5945938e340520d98d4afa89ad8cf681da8f4f6
FabioStorer <fabiostorer19@gmail.com>
2024-08-07 09:55:43 -0300

    feat: adicionado crud dos alunos com relação com curso

---
+++ aluno.js
@@ -0,0 +1,90 @@
+const prompt = require('prompt-sync')();
+const curso = require('./curso.js');
+const db = [];
+let ultimoId = 0;
+
+const model = (id = ++ultimoId) => {
+
+    const nome = prompt('Digite o nome do aluno: ');
+    let idCurso = 0;
+
+    if (curso.index()) {
+        idCurso = parseInt(prompt('Digite o ID do curso: '));
+    } else {
+        console.log('Cadastre um curso antes de realizar essa ação!');
+    }
+
+    if (nome != '' &&
+        curso.show(idCurso)) {
+        return {
+            id,
+            nome,
+            idCurso
+        };
+    }
+    console.log('Dados inválidos!');
+};
+
+const store = () => {
+
+    const novo = model();
+
+    if (novo) {
+        db.push(novo);
+        console.log('Registro concluído com sucesso!')
+    }
+};
+
+const index = () => {
+
+    if (db.length == 0) {
+        console.log('Nenhum curso cadastrado.');
+        return false;
+    }
+
+    db.forEach(el => console.log(el));
+    return true;
+};
+
+const show = id => db.find(el => el.id == id);
+
+const update = () => {
+
+    if (index()) {
+        const id = parseInt(prompt('Escolha pelo ID qual registro deseja atualizar: '));
+        const indice = db.findIndex(el => el.id == id);
+
+        if (indice != -1) {
+            const novo = model();
+            if (novo) {
+                db[indice] = novo;
+                console.log('Registro atualizado com sucesso!');
+            }
+        } else {
+            console.log('ID incorreto!');
+        }
+    }
+};
+
+const destroy = () => {
+
+    if (index()) {
+        const id = parseInt(prompt('Escolha pelo ID qual registro deseja remover: '));
+        const indice = db.findIndex(el => el.id == id);
+
+        if (indice != -1) {
+            db.splice(indice, 1);
+            console.log('Registro removido com sucesso!');
+        } else {
+            console.log('ID incorreto!');
+        }
+    }
+};
+
+module.exports = {
+    store,
+    index,
+    show,
+    update,
+    destroy
+};
