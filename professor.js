4503558db9defdb45021127425685f06037d5b73
FabioStorer <fabiostorer19@gmail.com>
2024-08-07 09:30:43 -0300

    feat: adicionado crud de professor

---
+++ professor.js
@@ -0,0 +1,79 @@
+const prompt = require('prompt-sync')();
+const db = [];
+let ultimoId = 0;
+
+const model = (id = ++ultimoId) => {
+
+    const nome = prompt('Digite o nome do professor: ');
+
+    if (nome != '') {
+        return {
+            id,
+            nome
+        };
+    }
+};
+
+const store = () => {
+
+    const novo = model();
+
+    if (novo) {
+        db.push(novo);
+    }
+    console.log('Dados inválidos!');
+};
+
+const index = () => {
+
+    if (db.length == 0) {
+        console.log('Nenhum professor cadastrado.');
+        return false;
+    }
+
+    db.forEach(el => console.log(el));
+    return true;
+};
+
+const show = id => db.find(el => el.id == id);
+
+const update = () => {
+
+    if (index()) {
+        const id = parseInt(prompt('Escolha pelo ID qual registro deseja atualizar: '));
+        const indice = db.findIndex(el => el.id == id);
+
+        if (indice != -1) {
+            const novo = model();
+            if (novo) {
+                db[indice] = novo;
+                console.log('Registro atualizado com sucesso!');
+            }
+        } else {
+            console.log('ID incorreto!');
+        }
+    }
+};
+
+const destroy = () => {
+
+    if (index()) {
+        const id = parseInt(prompt('Escolha pelo ID qual registro deseja remover: '));
+        const indice = db.findIndex(el => el.id == id);
+
+        if (indice != -1) {
+            db.splice(indice, 1);
+            console.log('Registro removido com sucesso!');
+        } else {
+            console.log('ID incorreto!');
+        }
+    }
+};
+
+module.exports = {
+    store,
+    index,
+    show,
+    update,
+    destroy
+};
